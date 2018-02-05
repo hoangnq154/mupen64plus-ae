fc9a0291c314936a85d4f4cf187dc57123dcd290
Francisco Zurita <fzurita@gmail.com>
2018-02-05 01:05:01 -0500

    front: Change application ID to org.mupen64plusae.v3.fzurita

--- app/src/main/java/paulscode/android/mupen64plusae/task/DeleteFilesService.java
+++ app/src/main/java/paulscode/android/mupen64plusae/task/DeleteFilesService.java
@@ -37,7 +37,7 @@ import android.os.Message;
 import android.os.Process;
 import androidx.core.app.NotificationCompat;
 
-import org.mupen64plusae.v3.alpha.R;
+import org.mupen64plusae.v3.fzurita.R;
 
 import java.io.File;
 import java.util.ArrayList;
