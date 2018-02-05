86c44b54a3f40b99354dc820c94dc0543dc3638c
Francisco Zurita <fzurita@gmail.com>
2018-02-05 01:05:01 -0500

    front: Change application ID to org.mupen64plusae.v3.fzurita

--- app/src/main/java/paulscode/android/mupen64plusae/persistent/GamePrefs.java
+++ app/src/main/java/paulscode/android/mupen64plusae/persistent/GamePrefs.java
@@ -5,7 +5,7 @@ import android.content.SharedPreferences;
 import android.text.TextUtils;
 import android.util.Log;
 
-import org.mupen64plusae.v3.alpha.R;
+import org.mupen64plusae.v3.fzurita.R;
 
 import java.io.File;
 import java.util.Collections;
