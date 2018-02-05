d8102fda987ff99487cb6d5c4663e07a490ae5ec
Francisco Zurita <fzurita@gmail.com>
2018-02-05 01:05:01 -0500

    front: Change application ID to org.mupen64plusae.v3.fzurita

--- app/src/main/java/paulscode/android/mupen64plusae/cheat/CheatEditorActivity.java
+++ app/src/main/java/paulscode/android/mupen64plusae/cheat/CheatEditorActivity.java
@@ -37,7 +37,7 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
-import org.mupen64plusae.v3.alpha.R;
+import org.mupen64plusae.v3.fzurita.R;
 
 import java.io.File;
 import java.util.ArrayList;
